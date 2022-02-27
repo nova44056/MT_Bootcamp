e0db6ee7429844513b8f9a4eaf618b49d0bd5d69
Uy Seng <nova@Uys-MacBook-Air.local>
2022-02-27 14:32:07 -0600

    Assignment 2

--- 02-21-2022/SQLQuery_1.sql
+++ 02-21-2022/SQLQuery_1.sql
@@ -13,228 +13,228 @@ DROP TABLE IF EXISTS Users;
 DROP TABLE IF EXISTS Locations;
 
 
-CREATE TABLE Locations(
-    id INT IDENTITY(1,1),
-    name VARCHAR(250)
+-- CREATE TABLE Locations(
+--     id INT IDENTITY(1,1),
+--     name VARCHAR(250)
 
-    PRIMARY KEY(id)
-);
+--     PRIMARY KEY(id)
+-- );
 
-CREATE TABLE Users(
-    id INT IDENTITY(1,1),
-    name VARCHAR(250),
+-- CREATE TABLE Users(
+--     id INT IDENTITY(1,1),
+--     name VARCHAR(250),
 
-    PRIMARY KEY(id)
-);
+--     PRIMARY KEY(id)
+-- );
 
-CREATE TABLE Roles(
-    id INT IDENTITY(1,1),
-    name VARCHAR(250),
-    description VARCHAR(250)
+-- CREATE TABLE Roles(
+--     id INT IDENTITY(1,1),
+--     name VARCHAR(250),
+--     description VARCHAR(250)
 
-    PRIMARY KEY(id)
-);
+--     PRIMARY KEY(id)
+-- );
 
-CREATE TABLE UserRoles(
-    user_id INT NOT NULL,
-    role_id INT NOT NULL,
+-- CREATE TABLE UserRoles(
+--     user_id INT NOT NULL,
+--     role_id INT NOT NULL,
 
-    FOREIGN KEY(user_id) REFERENCES Users(id),
-    FOREIGN KEY(role_id) REFERENCES Roles(id)
-);
+--     FOREIGN KEY(user_id) REFERENCES Users(id),
+--     FOREIGN KEY(role_id) REFERENCES Roles(id)
+-- );
 
-CREATE TABLE Taverns(
-    id INT IDENTITY(1,1),
-    name VARCHAR(250),
-    num_of_floor INT,
-    location_id INT NOT NULL,
-    owner_id INT NOT NULL,
-
-
-    PRIMARY KEY(id),
-    FOREIGN KEY(location_id) REFERENCES Locations(id),
-    FOREIGN KEY(owner_id) REFERENCES Users(id)
-);
-
-CREATE TABLE Supplies(
-    id INT IDENTITY(1,1),
-    name VARCHAR(250),
-    unit VARCHAR(250)
-
-    PRIMARY KEY(id)
-);
-
-CREATE TABLE Inventories(
-    id INT IDENTITY(1,1),
-    supply_id INT NOT NULL,
-    tavern_id INT NOT NULL,
-    updated_at DATE,
-    supply_count INT
-
-    PRIMARY KEY(id)
-    FOREIGN KEY(supply_id) REFERENCES Supplies(id),
-    FOREIGN KEY(tavern_id) REFERENCES Taverns(id)
-);
-
-CREATE TABLE BasementRats(
-    id INT IDENTITY(1,1),
-    tavern_id INT NOT NULL,
-    name VARCHAR(250),
-
-    PRIMARY KEY(id),
-    FOREIGN KEY(tavern_id) REFERENCES Taverns(id)
-);
+-- CREATE TABLE Taverns(
+--     id INT IDENTITY(1,1),
+--     name VARCHAR(250),
+--     num_of_floor INT,
+--     location_id INT NOT NULL,
+--     owner_id INT NOT NULL,
 
 
-CREATE TABLE Services(
-    id INT IDENTITY(1,1),
-    tavern_id INT NOT NULL,
-    name VARCHAR(250),
+--     PRIMARY KEY(id),
+--     FOREIGN KEY(location_id) REFERENCES Locations(id),
+--     FOREIGN KEY(owner_id) REFERENCES Users(id)
+-- );
 
-    PRIMARY KEY(id),
-    FOREIGN KEY(tavern_id) REFERENCES Taverns(id)
-);
+-- CREATE TABLE Supplies(
+--     id INT IDENTITY(1,1),
+--     name VARCHAR(250),
+--     unit VARCHAR(250)
 
-CREATE TABLE Statuses(
-    id INT IDENTITY(1,1),
-    name VARCHAR(250)
+--     PRIMARY KEY(id)
+-- );
 
-    PRIMARY KEY(id)
-);
+-- CREATE TABLE Inventories(
+--     id INT IDENTITY(1,1),
+--     supply_id INT NOT NULL,
+--     tavern_id INT NOT NULL,
+--     updated_at DATE,
+--     supply_count INT
 
-CREATE TABLE ServiceStatuses(
-    service_id INT NOT NULL,
-    status_id INT NOT NULL,
+--     PRIMARY KEY(id)
+--     FOREIGN KEY(supply_id) REFERENCES Supplies(id),
+--     FOREIGN KEY(tavern_id) REFERENCES Taverns(id)
+-- );
 
-    FOREIGN KEY(service_id) REFERENCES Services(id),
-    FOREIGN KEY(status_id) REFERENCES Statuses(id)
-);
+-- CREATE TABLE BasementRats(
+--     id INT IDENTITY(1,1),
+--     tavern_id INT NOT NULL,
+--     name VARCHAR(250),
 
-CREATE TABLE SupplyReceivedRecords(
-    id INT IDENTITY(1,1),
-    supply_id INT NOT NULL,
-    tavern_id INT NOT NULL,
-    amount_received INT,
-    date_received DATE,
-
-    PRIMARY KEY(id),
-    FOREIGN KEY(supply_id) REFERENCES Supplies(id),
-    FOREIGN KEY(tavern_id) REFERENCES Taverns(id)
-);
-
-CREATE TABLE Sales(
-    id INT IDENTITY(1,1),
-    service_id INT NOT NULL,
-    guest_id INT NOT NULL,
-    tavern_id INT NOT NULL,
-    price INT,
-    date_purchased DATE,
-    amount_purchased INT
-
-    PRIMARY KEY(id),
-    FOREIGN KEY(service_id) REFERENCES Services(id),
-    FOREIGN KEY(guest_id) REFERENCES Users(id),
-    FOREIGN KEY(tavern_id) REFERENCES Taverns(id)
-);
-
--- seeding data
--- insert locations
-INSERT INTO Locations(name) VALUES('Main Street');
-INSERT INTO Locations(name) VALUES('Melton Street');
-INSERT INTO Locations(name) VALUES('Park Street');
-INSERT INTO Locations(name) VALUES('Church Street');
-INSERT INTO Locations(name) VALUES('Church Street'); 
-
--- insert users
-INSERT INTO Users(name) VALUES('John');
-INSERT INTO Users(name) VALUES('Jane');
-INSERT INTO Users(name) VALUES('Jack');
-INSERT INTO Users(name) VALUES('Jill');
-INSERT INTO Users(name) VALUES('Jenny');
-INSERT INTO Users(name) VALUES('Jeremy');
-INSERT INTO Users(name) VALUES('Josh');
-INSERT INTO Users(name) VALUES('Jasmin');
-INSERT INTO Users(name) VALUES('Jane');
-INSERT INTO Users(name) VALUES('Jack');
+--     PRIMARY KEY(id),
+--     FOREIGN KEY(tavern_id) REFERENCES Taverns(id)
+-- );
 
 
--- insert roles
-INSERT INTO Roles(name, description) VALUES('admin', 'admin');
-INSERT INTO Roles(name, description) VALUES('guest', 'guest');
+-- CREATE TABLE Services(
+--     id INT IDENTITY(1,1),
+--     tavern_id INT NOT NULL,
+--     name VARCHAR(250),
 
--- insert user roles
-INSERT INTO UserRoles(user_id, role_id) VALUES(1, 1);
-INSERT INTO UserRoles(user_id, role_id) VALUES(2, 1);
-INSERT INTO UserRoles(user_id, role_id) VALUES(3, 1);
-INSERT INTO UserRoles(user_id, role_id) VALUES(4, 1);
-INSERT INTO UserRoles(user_id, role_id) VALUES(5, 1);
-INSERT INTO UserRoles(user_id, role_id) VALUES(6, 2);
-INSERT INTO UserRoles(user_id, role_id) VALUES(7, 2);
-INSERT INTO UserRoles(user_id, role_id) VALUES(8, 2);
-INSERT INTO UserRoles(user_id, role_id) VALUES(9, 2);
-INSERT INTO UserRoles(user_id, role_id) VALUES(10, 2);
+--     PRIMARY KEY(id),
+--     FOREIGN KEY(tavern_id) REFERENCES Taverns(id)
+-- );
+
+-- CREATE TABLE Statuses(
+--     id INT IDENTITY(1,1),
+--     name VARCHAR(250)
+
+--     PRIMARY KEY(id)
+-- );
+
+-- CREATE TABLE ServiceStatuses(
+--     service_id INT NOT NULL,
+--     status_id INT NOT NULL,
+
+--     FOREIGN KEY(service_id) REFERENCES Services(id),
+--     FOREIGN KEY(status_id) REFERENCES Statuses(id)
+-- );
+
+-- CREATE TABLE SupplyReceivedRecords(
+--     id INT IDENTITY(1,1),
+--     supply_id INT NOT NULL,
+--     tavern_id INT NOT NULL,
+--     amount_received INT,
+--     date_received DATE,
+
+--     PRIMARY KEY(id),
+--     FOREIGN KEY(supply_id) REFERENCES Supplies(id),
+--     FOREIGN KEY(tavern_id) REFERENCES Taverns(id)
+-- );
+
+-- CREATE TABLE Sales(
+--     id INT IDENTITY(1,1),
+--     service_id INT NOT NULL,
+--     guest_id INT NOT NULL,
+--     tavern_id INT NOT NULL,
+--     price INT,
+--     date_purchased DATE,
+--     amount_purchased INT
+
+--     PRIMARY KEY(id),
+--     FOREIGN KEY(service_id) REFERENCES Services(id),
+--     FOREIGN KEY(guest_id) REFERENCES Users(id),
+--     FOREIGN KEY(tavern_id) REFERENCES Taverns(id)
+-- );
+
+-- -- seeding data
+-- -- insert locations
+-- INSERT INTO Locations(name) VALUES('Main Street');
+-- INSERT INTO Locations(name) VALUES('Melton Street');
+-- INSERT INTO Locations(name) VALUES('Park Street');
+-- INSERT INTO Locations(name) VALUES('Church Street');
+-- INSERT INTO Locations(name) VALUES('Church Street'); 
+
+-- -- insert users
+-- INSERT INTO Users(name) VALUES('John');
+-- INSERT INTO Users(name) VALUES('Jane');
+-- INSERT INTO Users(name) VALUES('Jack');
+-- INSERT INTO Users(name) VALUES('Jill');
+-- INSERT INTO Users(name) VALUES('Jenny');
+-- INSERT INTO Users(name) VALUES('Jeremy');
+-- INSERT INTO Users(name) VALUES('Josh');
+-- INSERT INTO Users(name) VALUES('Jasmin');
+-- INSERT INTO Users(name) VALUES('Jane');
+-- INSERT INTO Users(name) VALUES('Jack');
 
 
--- insert taverns
-INSERT INTO Taverns(name, num_of_floor, location_id, owner_id) VALUES('Tavern 1', 1, 1, 1);
-INSERT INTO Taverns(name, num_of_floor, location_id, owner_id) VALUES('Tavern 2', 2, 2, 2);
-INSERT INTO Taverns(name, num_of_floor, location_id, owner_id) VALUES('Tavern 3', 3, 3, 3);
-INSERT INTO Taverns(name, num_of_floor, location_id, owner_id) VALUES('Tavern 4', 4, 4, 4);
-INSERT INTO Taverns(name, num_of_floor, location_id, owner_id) VALUES('Tavern 5', 5, 5, 5);
+-- -- insert roles
+-- INSERT INTO Roles(name, description) VALUES('admin', 'admin');
+-- INSERT INTO Roles(name, description) VALUES('guest', 'guest');
 
--- insert supplies
-INSERT INTO Supplies(name, unit) VALUES('Beer', 'bottles');
-INSERT INTO Supplies(name, unit) VALUES('Wine', 'bottles');
-INSERT INTO Supplies(name, unit) VALUES('Whiskey', 'bottles');
-INSERT INTO Supplies(name, unit) VALUES('Rum', 'bottles');
-INSERT INTO Supplies(name, unit) VALUES('Vodka', 'bottles');
-INSERT INTO Supplies(name, unit) VALUES('Beer', 'bottles');
-
--- insert inventories
-INSERT INTO Inventories(supply_id, tavern_id, updated_at, supply_count) VALUES(1, 1, '2019-01-01', 10);
-INSERT INTO Inventories(supply_id, tavern_id, updated_at, supply_count) VALUES(2, 1, '2019-01-01', 10);
-INSERT INTO Inventories(supply_id, tavern_id, updated_at, supply_count) VALUES(3, 1, '2019-01-01', 10);
-INSERT INTO Inventories(supply_id, tavern_id, updated_at, supply_count) VALUES(4, 1, '2019-01-01', 10);
-INSERT INTO Inventories(supply_id, tavern_id, updated_at, supply_count) VALUES(5, 1, '2019-01-01', 10);
-
--- insert basement rats
-INSERT INTO BasementRats(tavern_id, name) VALUES(1, 'Basement Rat 1');
-INSERT INTO BasementRats(tavern_id, name) VALUES(2, 'Basement Rat 2');
-INSERT INTO BasementRats(tavern_id, name) VALUES(3, 'Basement Rat 3');
-INSERT INTO BasementRats(tavern_id, name) VALUES(4, 'Basement Rat 4');
-INSERT INTO BasementRats(tavern_id, name) VALUES(5, 'Basement Rat 5');
-
--- insert statuses
-INSERT INTO Statuses(name) VALUES('Available');
-INSERT INTO Statuses(name) VALUES('Unavailable');
-INSERT INTO Statuses(name) VALUES('Out of Stock');
-INSERT INTO Statuses(name) VALUES('In Stock')
-
--- insert services
-INSERT INTO Services(name, tavern_id) VALUES('Karaoke', 1);
-INSERT INTO Services(name, tavern_id) VALUES('Massage', 1);
-INSERT INTO Services(name, tavern_id) VALUES('Bar', 1);
-INSERT INTO Services(name, tavern_id) VALUES('Weapon', 1);
-INSERT INTO Services(name, tavern_id) VALUES('Food', 1);
+-- -- insert user roles
+-- INSERT INTO UserRoles(user_id, role_id) VALUES(1, 1);
+-- INSERT INTO UserRoles(user_id, role_id) VALUES(2, 1);
+-- INSERT INTO UserRoles(user_id, role_id) VALUES(3, 1);
+-- INSERT INTO UserRoles(user_id, role_id) VALUES(4, 1);
+-- INSERT INTO UserRoles(user_id, role_id) VALUES(5, 1);
+-- INSERT INTO UserRoles(user_id, role_id) VALUES(6, 2);
+-- INSERT INTO UserRoles(user_id, role_id) VALUES(7, 2);
+-- INSERT INTO UserRoles(user_id, role_id) VALUES(8, 2);
+-- INSERT INTO UserRoles(user_id, role_id) VALUES(9, 2);
+-- INSERT INTO UserRoles(user_id, role_id) VALUES(10, 2);
 
 
--- insert service statuses
-INSERT INTO ServiceStatuses(service_id, status_id) VALUES(1, 1);
-INSERT INTO ServiceStatuses(service_id, status_id) VALUES(2, 1);
-INSERT INTO ServiceStatuses(service_id, status_id) VALUES(3, 2);
-INSERT INTO ServiceStatuses(service_id, status_id) VALUES(4, 3);
-INSERT INTO ServiceStatuses(service_id, status_id) VALUES(5, 4);
+-- -- insert taverns
+-- INSERT INTO Taverns(name, num_of_floor, location_id, owner_id) VALUES('Tavern 1', 1, 1, 1);
+-- INSERT INTO Taverns(name, num_of_floor, location_id, owner_id) VALUES('Tavern 2', 2, 2, 2);
+-- INSERT INTO Taverns(name, num_of_floor, location_id, owner_id) VALUES('Tavern 3', 3, 3, 3);
+-- INSERT INTO Taverns(name, num_of_floor, location_id, owner_id) VALUES('Tavern 4', 4, 4, 4);
+-- INSERT INTO Taverns(name, num_of_floor, location_id, owner_id) VALUES('Tavern 5', 5, 5, 5);
 
--- insert supply received records
-INSERT INTO SupplyReceivedRecords(supply_id, tavern_id, amount_received, date_received) VALUES(1, 1, 10, '2019-01-01');
-INSERT INTO SupplyReceivedRecords(supply_id, tavern_id, amount_received, date_received) VALUES(2, 1, 10, '2019-01-01');
-INSERT INTO SupplyReceivedRecords(supply_id, tavern_id, amount_received, date_received) VALUES(3, 1, 10, '2019-01-01');
-INSERT INTO SupplyReceivedRecords(supply_id, tavern_id, amount_received, date_received) VALUES(4, 1, 10, '2019-01-01');
-INSERT INTO SupplyReceivedRecords(supply_id, tavern_id, amount_received, date_received) VALUES(5, 1, 10, '2019-01-01');
+-- -- insert supplies
+-- INSERT INTO Supplies(name, unit) VALUES('Beer', 'bottles');
+-- INSERT INTO Supplies(name, unit) VALUES('Wine', 'bottles');
+-- INSERT INTO Supplies(name, unit) VALUES('Whiskey', 'bottles');
+-- INSERT INTO Supplies(name, unit) VALUES('Rum', 'bottles');
+-- INSERT INTO Supplies(name, unit) VALUES('Vodka', 'bottles');
+-- INSERT INTO Supplies(name, unit) VALUES('Beer', 'bottles');
 
--- insert sales
-INSERT INTO Sales(service_id, tavern_id, guest_id, price, amount_purchased, date_purchased) VALUES(1, 1, 6, 10, 10, '2019-01-01');
-INSERT INTO Sales(service_id, tavern_id, guest_id, price, amount_purchased, date_purchased) VALUES(2, 1, 7, 20, 10, '2019-01-01');
-INSERT INTO Sales(service_id, tavern_id, guest_id, price, amount_purchased, date_purchased) VALUES(1, 1, 8, 30, 10, '2019-01-01');
-INSERT INTO Sales(service_id, tavern_id, guest_id, price, amount_purchased, date_purchased) VALUES(4, 1, 9, 40, 10, '2019-01-01');
-INSERT INTO Sales(service_id, tavern_id, guest_id, price, amount_purchased, date_purchased) VALUES(5, 1, 10, 50, 10, '2019-01-01');
+-- -- insert inventories
+-- INSERT INTO Inventories(supply_id, tavern_id, updated_at, supply_count) VALUES(1, 1, '2019-01-01', 10);
+-- INSERT INTO Inventories(supply_id, tavern_id, updated_at, supply_count) VALUES(2, 1, '2019-01-01', 10);
+-- INSERT INTO Inventories(supply_id, tavern_id, updated_at, supply_count) VALUES(3, 1, '2019-01-01', 10);
+-- INSERT INTO Inventories(supply_id, tavern_id, updated_at, supply_count) VALUES(4, 1, '2019-01-01', 10);
+-- INSERT INTO Inventories(supply_id, tavern_id, updated_at, supply_count) VALUES(5, 1, '2019-01-01', 10);
+
+-- -- insert basement rats
+-- INSERT INTO BasementRats(tavern_id, name) VALUES(1, 'Basement Rat 1');
+-- INSERT INTO BasementRats(tavern_id, name) VALUES(2, 'Basement Rat 2');
+-- INSERT INTO BasementRats(tavern_id, name) VALUES(3, 'Basement Rat 3');
+-- INSERT INTO BasementRats(tavern_id, name) VALUES(4, 'Basement Rat 4');
+-- INSERT INTO BasementRats(tavern_id, name) VALUES(5, 'Basement Rat 5');
+
+-- -- insert statuses
+-- INSERT INTO Statuses(name) VALUES('Available');
+-- INSERT INTO Statuses(name) VALUES('Unavailable');
+-- INSERT INTO Statuses(name) VALUES('Out of Stock');
+-- INSERT INTO Statuses(name) VALUES('In Stock')
+
+-- -- insert services
+-- INSERT INTO Services(name, tavern_id) VALUES('Karaoke', 1);
+-- INSERT INTO Services(name, tavern_id) VALUES('Massage', 1);
+-- INSERT INTO Services(name, tavern_id) VALUES('Bar', 1);
+-- INSERT INTO Services(name, tavern_id) VALUES('Weapon', 1);
+-- INSERT INTO Services(name, tavern_id) VALUES('Food', 1);
+
+
+-- -- insert service statuses
+-- INSERT INTO ServiceStatuses(service_id, status_id) VALUES(1, 1);
+-- INSERT INTO ServiceStatuses(service_id, status_id) VALUES(2, 1);
+-- INSERT INTO ServiceStatuses(service_id, status_id) VALUES(3, 2);
+-- INSERT INTO ServiceStatuses(service_id, status_id) VALUES(4, 3);
+-- INSERT INTO ServiceStatuses(service_id, status_id) VALUES(5, 4);
+
+-- -- insert supply received records
+-- INSERT INTO SupplyReceivedRecords(supply_id, tavern_id, amount_received, date_received) VALUES(1, 1, 10, '2019-01-01');
+-- INSERT INTO SupplyReceivedRecords(supply_id, tavern_id, amount_received, date_received) VALUES(2, 1, 10, '2019-01-01');
+-- INSERT INTO SupplyReceivedRecords(supply_id, tavern_id, amount_received, date_received) VALUES(3, 1, 10, '2019-01-01');
+-- INSERT INTO SupplyReceivedRecords(supply_id, tavern_id, amount_received, date_received) VALUES(4, 1, 10, '2019-01-01');
+-- INSERT INTO SupplyReceivedRecords(supply_id, tavern_id, amount_received, date_received) VALUES(5, 1, 10, '2019-01-01');
+
+-- -- insert sales
+-- INSERT INTO Sales(service_id, tavern_id, guest_id, price, amount_purchased, date_purchased) VALUES(1, 1, 6, 10, 10, '2019-01-01');
+-- INSERT INTO Sales(service_id, tavern_id, guest_id, price, amount_purchased, date_purchased) VALUES(2, 1, 7, 20, 10, '2019-01-01');
+-- INSERT INTO Sales(service_id, tavern_id, guest_id, price, amount_purchased, date_purchased) VALUES(1, 1, 8, 30, 10, '2019-01-01');
+-- INSERT INTO Sales(service_id, tavern_id, guest_id, price, amount_purchased, date_purchased) VALUES(4, 1, 9, 40, 10, '2019-01-01');
+-- INSERT INTO Sales(service_id, tavern_id, guest_id, price, amount_purchased, date_purchased) VALUES(5, 1, 10, 50, 10, '2019-01-01');
---
+++ 02-24-2022/assignment2.sql
@@ -0,0 +1,100 @@
+IF NOT EXISTS(SELECT * FROM sys.databases WHERE name = 'mt_bootcamp_assignment_2_uy_seng')
+    BEGIN
+        CREATE DATABASE mt_bootcamp_assignment_2_uy_seng;
+    END;    
+USE mt_bootcamp_assignment_2_uy_seng;
+
+-- will cause foreign key constraint
+-- DROP TABLE Roles;
+
+DROP TABLE IF EXISTS SupplySales, Sales, Supplies, Levels, Classes, Guests, GuestStatuses, BasementRats, Taverns, Locations, Users, Roles;
+
+CREATE TABLE Roles(
+    id INT PRIMARY KEY,
+    name VARCHAR(50),
+    description VARCHAR(MAX)
+)
+
+
+CREATE TABLE Users(
+    id INT IDENTITY(1,1) PRIMARY KEY,
+    name VARCHAR(250),
+    role_id INT NOT NULL
+)
+
+ALTER TABLE Users ADD FOREIGN KEY(role_id) REFERENCES Roles(id);
+
+CREATE TABLE Locations(
+    id INT IDENTITY(1,1) PRIMARY KEY,
+    name VARCHAR(100)
+)
+
+CREATE TABLE Taverns(
+    id INT IDENTITY(1,1) PRIMARY KEY,
+    name VARCHAR(250),
+    location_id INT,
+    owner_id INT,
+)
+
+ALTER TABLE Taverns ADD FOREIGN KEY(location_id) REFERENCES Locations(id);
+ALTER TABLE Taverns ADD FOREIGN KEY(owner_id) REFERENCES Users(id);
+
+CREATE TABLE BasementRats(
+    id INT IDENTITY(1,1) PRIMARY KEY,
+    name VARCHAR(100)
+)
+
+DROP TABLE BasementRats;
+
+CREATE TABLE GuestStatuses(
+    id INT IDENTITY(1,1) PRIMARY KEY,
+    name VARCHAR(50)
+)
+
+CREATE TABLE Guests(
+    id INT IDENTITY(1,1) PRIMARY KEY,
+    name VARCHAR(250),
+    notes VARCHAR(MAX),
+    birth_date DATE,
+    cake_day DATE,
+    status_id INT,
+    
+    FOREIGN KEY(status_id) REFERENCES GuestStatuses(id)
+)
+
+CREATE TABLE Classes(
+    id INT IDENTITY(1,1) PRIMARY KEY,
+    name VARCHAR(50)
+
+)
+
+CREATE TABLE Levels(
+    guest_id INT,
+    class_id INT,
+    date DATE
+
+    FOREIGN KEY(guest_id) REFERENCES Guests(id),
+    FOREIGN KEY(class_id) REFERENCES Classes(id)
+)
+
+CREATE TABLE Supplies(
+    id INT IDENTITY(1,1) PRIMARY KEY,
+    name VARCHAR(250),
+)
+
+CREATE TABLE Sales(
+    id INT IDENTITY(1,1) PRIMARY KEY,
+    tavern_id INT,
+    amount_received INT,
+    date DATE
+
+    FOREIGN KEY(tavern_id) REFERENCES Taverns(id)
+)
+
+CREATE TABLE SupplySales(
+    supply_id INT,
+    sale_id INT,
+
+    FOREIGN KEY(supply_id) REFERENCES Supplies(id),
+    FOREIGN KEY(sale_id) REFERENCES Sales(id)
+)
